461d313219893a0c194bc13e5325a051d6d5e758
David <David@192.168.0.16>
2021-12-15 12:35:20 -0800

    Initial Commit

---
+++ src/main/java/com/wine/core/WattApplication.java
@@ -0,0 +1,13 @@
+package com.wine.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WattApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WattApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wine/core/WattApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wine.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WattApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
